0b42d606138269f416ea91b6b890f82aebbc9b29
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2021-10-24 03:55:18 +0330

    Refactor BufferedReader

--- src/BufferedReader.py
+++ src/BufferedReader.py
@@ -3,60 +3,33 @@ class BufferedReader:
         self.filename = filename
         self.file = open(filename, 'r')
         self.half_buffer_size = half_buffer_size
-        self.buffer = [''] * ((self.half_buffer_size + 1) * 2)
+        self.buffer = [""] * 2
         self.forward = 0
-        read_string = self.file.read(self.half_buffer_size)
-        self.beginnig_retreat = False
-        self.buffer[:len(read_string)] = read_string
+        self.current_half = 1 # So goto_next_half_buffer goes to 0
+        self.must_fill_next_half_buffer = True
+        self.goto_next_half_buffer()
 
-    def fill_buffer(self, read_string, half):
-        if half == 1:
-            for i in range(len(read_string)):
-                self.buffer[i] = read_string[i]
-            self.buffer[len(read_string)] = ''
-        elif half == 2:
-            for i in range(len(read_string)):
-                self.buffer[i + self.half_buffer_size + 1] = read_string[i]
-            self.buffer[len(read_string) + self.half_buffer_size + 1] = ''
+    def goto_next_half_buffer(self):
+        self.current_half = (self.current_half + 1) % 2
+        self.forward = 0
+        if self.must_fill_next_half_buffer:
+            self.buffer[self.current_half] = self.file.read(self.half_buffer_size)
+            self.has_next = (self.buffer[self.current_half] != "")
+        self.must_fill_next_half_buffer = True
 
     def retreat(self):
-        if self.forward == self.half_buffer_size + 1:
-            self.forward -= 1
-            self.beginnig_retreat = True
         if self.forward == 0:
-            self.forward = self.half_buffer_size * 2 + 1
-            self.beginnig_retreat = True
-        self.forward -= 1
+            self.current_half = (self.current_half + 1) % 2
+            self.forward = len(self.buffer[self.current_half]) - 1
+            self.must_fill_next_half_buffer = False
+        else:
+            self.forward -= 1
 
     def next_char(self):
-        retval = self.buffer[self.forward]
+        retval = self.buffer[self.current_half][self.forward]
         self.forward += 1
-        if not self.beginnig_retreat:
-            if self.buffer[self.forward] == '':
-                if self.forward == self.half_buffer_size:
-                    read_string = self.file.read(self.half_buffer_size)
-                    self.fill_buffer(read_string, 2)
-                    self.forward += 1
-                    if read_string == '':
-                        self.file.close()
-                        return retval, False
-                elif self.forward == self.half_buffer_size * 2 + 1:
-                    read_string = self.file.read(self.half_buffer_size)
-                    if read_string == '':
-                        self.file.close()
-                        return retval, False
-                    self.fill_buffer(read_string, 1)
-                    self.forward = 0
-                else:
-                    self.file.close()
-                    return retval, False
-        else:
-            if self.forward == self.half_buffer_size:
-                self.forward += 1
-                self.beginnig_retreat = False
-            elif self.forward == self.half_buffer_size * 2 + 1:
-                self.forward = 0
-                self.beginnig_retreat = False
-            else:
-                raise Exception("Should Not Reach this state")
-        return retval, True
+        if self.forward == len(self.buffer[self.current_half]):
+            self.goto_next_half_buffer()
+            if not self.has_next:
+                self.file.close()
+        return retval, self.has_next
