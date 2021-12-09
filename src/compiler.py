faaad362d1333a1c408fab874736510c79846834
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2021-12-10 01:56:35 +0330

    Minor refinements
    Co-Authored-By: Amirmahdi Namjoo <amirm137878@gmail.com>

--- src/compiler.py
+++ src/compiler.py
@@ -1,7 +1,5 @@
 import states
 from lexer import Lexer
-from lexer_output_generator import *
-from lexer_file_writer import *
 from cminus_parser import Parser
 from parse_tree_generator import *
 
@@ -10,6 +8,8 @@ if __name__ == '__main__':
     lexer = Lexer()
     parser = Parser(lexer)
     initial_node, errors = parser.parse()
+    if len(errors) == 0:
+        errors = ["There is no syntax error."]
     tree = get_tree(initial_node)
     with open("parse_tree.txt", "w") as f:
         print(render_tree(tree), file=f)
