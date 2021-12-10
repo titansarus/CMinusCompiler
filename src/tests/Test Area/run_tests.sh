687da40d6132f925516afe784ad5167a40fa7a1f
Amirmahdi Namjoo <amirm137878@gmail.com>
2021-12-10 16:22:09 +0330

    Fix bash script

--- src/tests/Test Area/run_tests.sh
+++ src/tests/Test Area/run_tests.sh
@@ -2,20 +2,19 @@
 echo "" > log.txt
 echo "" > brief_results.txt
 #for dir in ../PA1_final_tests/*; do
-for dir in ../PA1_input_output_samples/*; do
+#for dir in ../PA1_input_output_samples/*; do
+for dir in ../PA2_testcases/*; do
     cp "${dir}/input.txt" ./input.txt
-    python3 ../../compiler.py
+    python ../../compiler.py
     printf "\n\n\n\n=====================================>>>>> Running Test ${dir}...\n" >> log.txt
     printf "\n\n=====================================>>>>> Running Test ${dir}...\n" >> brief_results.txt
-    printf "\n\n              *** tokens.txt diffrences ***\n" >> log.txt
-    diff -y -B -W 250 -w  --suppress-common-lines ./tokens.txt "${dir}/tokens.txt" >> log.txt
-    diff -y -B -W 250 -w -q ./tokens.txt "${dir}/tokens.txt" >> brief_results.txt
-    printf "\n\n              *** lexical_errors.txt diffrences ***\n" >> log.txt
-    diff -y -B -W 250 -w  --suppress-common-lines ./lexical_errors.txt "${dir}/lexical_errors.txt" >> log.txt
-    diff -y -B -W 250 -w -q ./lexical_errors.txt "${dir}/lexical_errors.txt" >> brief_results.txt
-    printf "\n\n              *** symbol_table.txt diffrences ***\n" >> log.txt
-    diff -y -B -W 250 -w  --suppress-common-lines ./symbol_table.txt "${dir}/symbol_table.txt" >> log.txt
-    diff -y -B -W 250 -w -q ./symbol_table.txt "${dir}/symbol_table.txt" >> brief_results.txt
+    printf "\n\n              *** syntax_errors.txt diffrences ***\n" >> log.txt
+    diff -y -B -W 250 -w  --suppress-common-lines ./syntax_errors.txt "${dir}/syntax_errors.txt" >> log.txt
+    diff -y -B -W 250 -w -q ./syntax_errors.txt "${dir}/syntax_errors.txt" >> brief_results.txt
+    printf "\n\n              *** parse_tree.txt diffrences ***\n" >> log.txt
+    diff -y -B -W 250 -w  --suppress-common-lines ./parse_tree.txt "${dir}/parse_tree.txt" >> log.txt
+    diff -y -B -W 250 -w -q ./parse_tree.txt "${dir}/parse_tree.txt" >> brief_results.txt
+
 done
 
 
