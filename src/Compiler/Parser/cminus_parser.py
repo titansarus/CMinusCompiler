3a39c23193890227e889bfd57d526c3efcef7f2c
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2021-12-10 21:58:04 +0330

    Rollback to "missing $"

--- src/Compiler/Parser/cminus_parser.py
+++ src/Compiler/Parser/cminus_parser.py
@@ -140,7 +140,6 @@ class ProductionParser:
         message = e.args[0]
         lexeme = e.args[1]
         token: Token = e.args[2]
-        lexer: Lexer = e.args[3]
         error_message = f"#{token.lineno} : syntax error, {message} {lexeme}"
         if token.lexeme == "$":
             message = "Unexpected EOF"
@@ -148,10 +147,6 @@ class ProductionParser:
             file_ended = True
             errors.append(error_message)
             return False
-        elif message == "missing" and lexeme == "$":
-            while token.lexeme != "$":
-                errors.append(f"#{token.lineno} : syntax error, illegal {token.lexeme}")
-                token = get_next_valid_token(lexer)
         errors.append(error_message)
         return True
 
@@ -160,11 +155,11 @@ class ProductionParser:
         global current_token
         if is_nonterminal_missing:
             self.current_state = error_edge.destination
-            raise Exception("missing", error_edge.label.name, current_token, self.lexer)
+            raise Exception("missing", error_edge.label.name, current_token)
 
         elif is_NUM_or_ID_missing or is_KEYWORD_or_SYMBOL_missing:
             self.current_state = error_edge.destination
-            raise Exception("missing", error_edge.label, current_token, self.lexer)
+            raise Exception("missing", error_edge.label, current_token)
 
         elif is_token_illegal:
             illegal_token = current_token
@@ -172,4 +167,4 @@ class ProductionParser:
             illegal_lexeme = illegal_token.lexeme
             if illegal_token.token_type in [ID, NUM]:
                 illegal_lexeme = illegal_token.token_type
-            raise Exception("illegal", illegal_lexeme, illegal_token, self.lexer)
+            raise Exception("illegal", illegal_lexeme, illegal_token)
